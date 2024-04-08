6f4248081cbde7bea067337a5b45d5d34b2790b0
lunafl1are <lunafl1are@protonmail.com>
2024-04-08 18:52:25 +0300

    improve error messages

---
+++ Task-06-07/JUnitProject/src/a_Introductory/Point.java
@@ -0,0 +1,20 @@
+package a_Introductory;
+
+public class Point {
+
+	public final Integer x, y;
+	
+	public Point(Integer x, Integer y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Point add(Point p) {
+		return new Point(x + p.x, y + p.y);
+	}
+	
+	public Point sub(Point p) {
+		return new Point(x - p.x, y - p.y);
+	}
+
+}
